dfd6cb668ef90ab0b973fc579cc50000e973a0d5
Boderux <16thanh2003@gmail.com>
2024-03-11 09:48:29 +0700

    feat(project): change for testing sonarqube

--- src/main/java/com/boder/grocery/GroceryServerApplication.java
+++ src/main/java/com/boder/grocery/GroceryServerApplication.java
@@ -8,7 +8,7 @@ public class GroceryServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GroceryServerApplication.class, args);
-        System.out.print("Code lỏ");
+        System.out.print("Testing");
     }
 
 }
