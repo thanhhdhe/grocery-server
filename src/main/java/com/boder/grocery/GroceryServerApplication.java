7e2170abd6bd3dec7857555d693c9c8f057cdebc
Boderux <16thanh2003@gmail.com>
2024-03-11 09:31:02 +0700

    feat(project): init project

---
+++ src/main/java/com/boder/grocery/GroceryServerApplication.java
@@ -0,0 +1,14 @@
+package com.boder.grocery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryServerApplication.class, args);
+        System.out.print("Code lỏ");
+    }
+
+}
---
+++ src/test/java/com/boder/grocery/GroceryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boder.grocery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
